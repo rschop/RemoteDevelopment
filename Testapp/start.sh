80926a4bbfa1f620018c6eb26c8098335b0ac8a2
Richard Schop <raschop@outlook.com>
2022-08-27 06:14:40 +0200

    WSL aanpassingen

--- Testapp/start.sh
+++ Testapp/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# niet als sudo uitvoeren
 app="docker.test"
-docker build -t ${app} .
-docker run -d -p 56733:80 --name=${app} -v $PWD:/app ${app}
+docker build -t rschop/odcn:flaskapp .
+docker run -d -p 56733:80 --name=${app} -v $PWD:/app rschop/odcn:flaskapp
+docker push rschop/odcn:flaskapp
